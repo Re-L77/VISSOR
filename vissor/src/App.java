73ae6ebd50f7446d04b85b17daf6c74e57b5f8ba
Ordaz Magos Juan Pablo <124051537@upq.edu.mx>
2025-08-08 21:29:46 -0600

    ya quedo esta mamada

--- vissor/src/App.java
+++ vissor/src/App.java
@@ -1,18 +1,23 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
-    public void start(Stage stage) {
-        Label label = new Label("Hola, afafsdcd");
-        Scene scene = new Scene(label, 400, 200);
-        stage.setScene(scene);
-        stage.setTitle("Prueba");
-        stage.show();
+    public void start(Stage primaryStage) {
+    try {
+        Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+        Scene scene = new Scene(root);
+        
+        primaryStage.setTitle("VISSOR");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    } catch(Exception e) {
+        e.printStackTrace();
     }
-
+}
     public static void main(String[] args) {
         launch();
     }
